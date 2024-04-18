3d9f26b1eba328a66eea1e4b0c219344a6bf8b57
Nicholas-Fultz-student <144163154+Nicholas-Fultz-student@users.noreply.github.com>
2024-04-18 14:28:48 -0400

    Initial Push Lamps
    Pushing

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    private float xAxis;
+    private float yAxis;
+
+    float xAxisTurnRate = 360f;
+    float yAxisTurnRate = 360f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+
+        Quaternion newRotation = Quaternion.Euler(xAxis, yAxis, 0f);
+
+        Camera.main.transform.rotation = newRotation;
+    }
+
+    public void AddXAxisInput(float input)
+    {
+        xAxis -= input * xAxisTurnRate;
+        xAxis = Mathf.Clamp(xAxis, -87f, 87f);
+        
+    }
+
+    public void AddYAxisInput(float input)
+    {
+        yAxis += input * yAxisTurnRate;
+    }
+}
